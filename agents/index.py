8fdff3635daefe9df4ae21b8f6f5fa7e8c799d81
zhenggl <kbmjj123@gmail.com>
2025-07-15 08:04:52 +0800

    refactor(agent): 重构智能助手并优化提示词生成逻辑
    - 更新 AgentExecutor 以使用 create_react_agent
    - 重构 generate_prompt 函数，移除用户输入和当前时间参数
    - 在 prompts/constants.py 中添加工具调用说明- 更新 exec_agent 函数，增加当前时间和聊天历史记录参数

--- agents/index.py
+++ agents/index.py
@@ -1,21 +1,22 @@
-from langchain.agents import initialize_agent, AgentType
+from langchain.agents import AgentExecutor, create_react_agent
 from tools import generate_tools
 from models import generate_llm
 from prompts import generate_prompt
+from utils import get_current_local_datetime
+
 
 def exec_agent(user_input, debug_info, chat_history):
     tools = generate_tools()
     llm = generate_llm()
-    agent = initialize_agent(
-        tools = tools,
-        llm = llm,
-        agent = AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-        verbose = True
-    )
-    prompt = generate_prompt(user_input)
-    agent.invoke({
-        "input": prompt
+    current_time = get_current_local_datetime()
+    prompt = generate_prompt()
+    agent = create_react_agent(llm=llm, tools=tools, prompt=prompt)
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+    agent_executor.invoke({
+        "input": user_input,
+        "current_time": current_time,
+        "chat_history": "\n".join(chat_history)
     })
 
 if __name__ == "__main__":
-    exec_agent("我要去北京", "", [])
+    exec_agent("请帮我指定一个广东汕头7日双人游的旅游方案", "", [])
--- prompts/index.py
+++ prompts/index.py
@@ -1,16 +1,14 @@
 from langchain_core.prompts import PromptTemplate
 
 from prompts.constants import agent_prompt_template
-from utils import get_current_local_datetime
 
 
-def generate_prompt(user_input: str):
-    current_time = get_current_local_datetime()
+def generate_prompt():
     """生成全局唯一提示词"""
     prompt_template = PromptTemplate.from_template(
         template=agent_prompt_template,
     )
-    return prompt_template.format(user_input=user_input, current_time=current_time)
+    return prompt_template
 
 if __name__ == '__main__':
     print(generate_prompt("我要去北京"))
